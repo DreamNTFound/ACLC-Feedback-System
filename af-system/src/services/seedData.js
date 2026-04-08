d0a8eca75e911707d87fac8bb3ee6fe9dfe84bb1
DreamNTFound <jenardakatsuki@gmail.com>
2026-04-08 12:36:08 +0800

    add usn temporary data to fetch

--- af-system/src/services/seedData.js
+++ af-system/src/services/seedData.js
@@ -3,45 +3,50 @@ export const seedFeedback = () => {
     const initialFeedback = [
       {
         id: 1,
+        usn: "12345678",
         title: "Teaching Quality Improved",
         feedback:
           "The teaching quality has improved significantly this semester.",
         category: "Teaching",
-        type: "👍",
+        type: "👍 Positive",
         created_at: "03-25-2025",
       },
       {
         id: 2,
+        usn: "87654321",
         title: "Administration Issues",
         feedback:
           "There are several issues with the administration's handling of student concerns.",
         category: "Administration",
-        type: "⚠️",
+        type: "⚠️ Concern",
         created_at: "03-25-2025",
       },
       {
         id: 3,
+        usn: "43215678",
         title: "More Tech Support Needed",
         feedback: "The faculty needs to provide more tech support to students.",
         category: "Course Content",
-        type: "💡",
+        type: "💡 Suggestion",
         created_at: "03-25-2025",
       },
       {
         id: 4,
+        usn: "87651234",
         title: "Facilities Upgrade Needed",
         feedback:
           "The facilities need to be upgraded to improve student experience.",
         category: "Facilities",
-        type: "💬",
+        type: "💬 Neutral",
         created_at: "03-25-2025",
       },
       {
         id: 5,
+        usn: "12348765",
         title: "Great Course Content",
         feedback: "The course content was very engaging and informative.",
         category: "Course Content",
-        type: "👍",
+        type: "👍 Positive",
         created_at: "03-25-2025",
       },
     ];
